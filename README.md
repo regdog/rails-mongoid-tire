d6f8c4b4d7d51b61115a2b13ccf6d0eb98aeb9a5
Tom Stoecklein <tom@vexea.com>
2012-11-18 04:02:40 -0500

    meet rubber

---
+++ README.md
@@ -0,0 +1,4 @@
+# Meet the Rubber App
+Rubber is a sample Rails 3.2 app with [Mongoid](http://mongoid.org) and Tire (http://github.com/karmi/tire) for full-text search via the incredibly powerful (and incredibly cool) [Elasticsearch](http://elasticsearch.org).
+
+Right now, Rubber is really just a WIP as I write the accompanying tutorial. Stay tuned.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubber::Application.config.secret_token = 'a1c5b05ec6f5118e874fd1bd9e2a08abaeb3994f7306da9c26c43608b3067ba29b7dc218992b1ba11f5e6c1036a69f20388fd8b6729beb801227387a4a6169ea'
